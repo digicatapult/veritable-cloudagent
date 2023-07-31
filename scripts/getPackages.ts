b7f915ccb5bad7963f4910961fe1858f27f66090
dc-andysign <andrei.baloiu@gmail.com>
2023-07-31 18:07:37 +0100

    Small fix -- lint

--- scripts/getPackages.ts
+++ scripts/getPackages.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-const packages = [ 'rest' ]
+const packages = ['rest']
 
 console.log(`packages: ${JSON.stringify(packages)}`)
 console.log(`::set-output name=packages::${JSON.stringify(packages)}`)
