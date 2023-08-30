26a42e9eeef3137ee4a6671320df223368e77da7
Ellenn-A <58051865+Ellenn-A@users.noreply.github.com>
2023-08-30 14:08:59 +0100

    Feature/ver 72 (#27)
    - added schema 
    - added tests for schema and credential test creation and fetching

--- tests/credentialDefinition.test.ts
+++ tests/credentialDefinition.test.ts
@@ -12,6 +12,9 @@ import { setupServer } from '../src/server'
 
 import { getTestAgent, getTestCredDef, getTestSchema } from './utils/helpers'
 
+import _schema from '../schema/schemaAttributes.json'
+const schema = _schema as AnonCredsSchema
+
 describe('CredentialDefinitionController', () => {
   let app: Express
   let agent: RestAgent
@@ -82,9 +85,44 @@ describe('CredentialDefinitionController', () => {
     })
 
     test('should return 404 NotFound when credential definition not found', async () => {
+      const spy = stub(agent.modules.anoncreds, 'getCredentialDefinition')
+      spy.resolves({
+        credentialDefinitionId: 'x',
+        credentialDefinitionMetadata: {},
+        resolutionMetadata: {
+          error: 'notFound',
+        },
+      })
+
       const id = encodeURIComponent('ipfs://QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR')
       const response = await request(app).get(`/credential-definitions/${id}`)
       expect(response.statusCode).to.be.equal(404)
+    })
+  })
+
+  describe('get credential definition by id using schema definition json', () => {
+    test('should return credential definition ', async () => {
+      testCredDef.schemaId = 'ipfs://bafkreihiz6z2hcostlo73fycbflmhsayoyuqkicwinpqc7knxwmhhwahqq'
+      const spy = stub(agent.modules.anoncreds, 'getCredentialDefinition')
+      spy.resolves({
+        credentialDefinitionId: 'WgWxqztrNooG92RXvxSTWv:3:CL:20:tag',
+        credentialDefinitionMetadata: {},
+        resolutionMetadata: {},
+        credentialDefinition: testCredDef,
+      })
+      const getResult = () => ({
+        id: 'WgWxqztrNooG92RXvxSTWv:3:CL:20:tag',
+        ...testCredDef,
+      })
+
+      const response = await request(app).get(`/credential-definitions/WgWxqztrNooG92RXvxSTWv:3:CL:20:tag`)
+      const result = await getResult()
+
+      expect(response.statusCode).to.be.equal(200)
+      expect(response.body.id).to.deep.equal(result.id)
+      expect(response.body.schemaId).to.deep.equal(result.schemaId)
+      expect(response.body.tag).to.deep.equal(result.tag)
+      expect(response.body.type).to.deep.equal(result.type)
     })
   })
 
@@ -137,6 +175,46 @@ describe('CredentialDefinitionController', () => {
         tag: 'latest',
       })
       expect(response.statusCode).to.be.equal(422)
+    })
+  })
+
+  describe('create credential definition using new json schema ', () => {
+    test('should return created credential definitionusing new json ', async () => {
+      const registerSchemaStub = stub(agent.modules.anoncreds, 'registerSchema')
+      const registerCredentialDefinitionStub = stub(agent.modules.anoncreds, 'registerCredentialDefinition')
+      // mock out getSchema from CredentialDefinitionController
+      const getSchemaStub = stub(agent.modules.anoncreds, 'getSchema').resolves({
+        resolutionMetadata: {},
+        schemaMetadata: {},
+        schemaId: 'ipfs://bafkreihiz6z2hcostlo73fycbflmhsayoyuqkicwinpqc7knxwmhhwahqq',
+        schema: schema,
+      })
+
+      testCredDef.schemaId = 'ipfs://bafkreihiz6z2hcostlo73fycbflmhsayoyuqkicwinpqc7knxwmhhwahqq'
+
+      registerCredentialDefinitionStub.resolves({
+        credentialDefinitionState: {
+          state: 'finished',
+          credentialDefinition: testCredDef,
+          credentialDefinitionId: `ipfs://bafkreiafhhldn47xkq4r5frf4lgaqqq35h66xikhttp5s7g5peotmi2szu`,
+        },
+        registrationMetadata: {},
+        credentialDefinitionMetadata: {},
+      })
+
+      const getCredentialDefResult = () => ({
+        id: 'ipfs://bafkreiafhhldn47xkq4r5frf4lgaqqq35h66xikhttp5s7g5peotmi2szu',
+        ...testCredDef,
+      })
+
+      const responseCredentialDeff = await request(app).post(`/credential-definitions/`).send({
+        issuerId: testCredDef.issuerId,
+        schemaId: testCredDef.schemaId,
+        tag: testCredDef.tag,
+      })
+
+      expect(responseCredentialDeff.statusCode).to.be.equal(200)
+      expect(responseCredentialDeff.body).to.deep.equal(getCredentialDefResult())
     })
   })
 
