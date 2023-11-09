9403139dadf0b456ef3380c395900fcad910d50d
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2023-11-09 18:58:30 +0000

    Remove platform selection

--- docker-rest-dev-setup.sh
+++ docker-rest-dev-setup.sh
@@ -2,7 +2,7 @@
 docker network create test --subnet 172.19.0.0/24
 
 # Build rest image
-cd ./packages/rest/ ; docker build --platform=linux/amd64 -f ./Dockerfile -t afj . ; cd ../..
+cd ./packages/rest/ ; docker build -f ./Dockerfile -t afj . ; cd ../..
 
 # Run aries
 docker run -d -it --network test -v $PWD:/root/ --name aries -h aries --platform linux/amd64 --ip 172.19.0.3 -p 5000 -p 3000 --entrypoint /bin/bash afj
@@ -10,7 +10,7 @@ docker run -d -it --network test -v $PWD:/root/ --name aries -h aries --platform
 # Echo how to attach aries container
 echo "docker exec -it aries bash"
 
-# Echo hot to test
+# Echo how to test
 echo "GENESIS_TXN_PATH=/root/network/genesis/docker-genesis.txn TEST_AGENT_PUBLIC_DID_SEED=000000000000000000000000Trustee9 npm run test"
 
 # KILL_ALL
