8abce365deca9ee9bf7002c65d1a6e23c99eb854
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2023-11-10 08:27:55 +0000

    Fix dev-setup test script

--- docker-rest-dev-setup.sh
+++ docker-rest-dev-setup.sh
@@ -1,11 +1,11 @@
+# Build docker image
+docker build -f ./Dockerfile -t afj .
+
 # Create network
 docker network create test --subnet 172.19.0.0/24
 
-# Build rest image
-cd ./packages/rest/ ; docker build -f ./Dockerfile -t afj . ; cd ../..
-
 # Run aries
-docker run -d -it --network test -v $PWD:/root/ --name aries -h aries --platform linux/amd64 --ip 172.19.0.3 -p 5000 -p 3000 --entrypoint /bin/bash afj
+docker run -d -it --network test -v $PWD:/root/ --name aries -h aries --ip 172.19.0.3 -p 5002 -p 3000 --entrypoint /bin/bash afj
 
 # Echo how to attach aries container
 echo "docker exec -it aries bash"
