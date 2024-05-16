1228e78854171b6556b3ed00c040dd810e82eb1e
Mike Sterle-Contala <2691351+msterle@users.noreply.github.com>
2024-05-16 11:36:22 +0100

    add send response controller

--- src/controllers/drpc/DrpcController.ts
+++ src/controllers/drpc/DrpcController.ts
@@ -18,7 +18,39 @@ export class DrpcController extends Controller {
   }
 
   /**
-   * sends the request object to the connection and returns the response
+   * sends a request object to the connection
+   *
+   * @param connectionId the connection to send the request to
+   * @param request The request object
+   * @param timeout The timeout for receiving a response
+   */
+  @Post('/:connectionId')
+  @Response<NotFound['message']>(404)
+  @Response<GatewayTimeout>(504)
+  public async sendRequest(
+    @Path('connectionId') connectionId: RecordId,
+    @Body() request: DrpcRequest,
+    @Query('async') async_ = false,
+    @Query('timeout') timeout = 5000
+  ) {
+    const responseListener = await this.agent.modules.drpc.sendRequest(connectionId, request)
+    const responsePromise = responseListener(timeout).then((response: DrpcResponse) => {
+      if (response === undefined) {
+        throw new GatewayTimeout('Response from peer timed out')
+      }
+      return response
+    })
+    let response: DrpcResponse | undefined
+    if (async_) {
+      this.setStatus(202)
+    } else {
+      response = await responsePromise
+    }
+    return response
+  }
+
+  /**
+   * sends a response object to the connection
    *
    * @param connectionId the connection to send the request to
    * @param request The request object
