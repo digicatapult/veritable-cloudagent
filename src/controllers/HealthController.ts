99b8765779da2a39c77f96fe88c1b8f02d92e72e
Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>
2024-06-10 15:40:45 +0100

    updated healthcontroller

--- src/controllers/HealthController.ts
+++ src/controllers/HealthController.ts
@@ -1,26 +1,27 @@
-import { Agent } from '@credo-ts/core'
-import { Controller, Get, Route, SuccessResponse, Tags } from 'tsoa'
+import { Controller, Get, Hidden, Route, SuccessResponse } from 'tsoa'
 import { injectable } from 'tsyringe'
 
 const packageVersion = process.env.npm_package_version ? process.env.npm_package_version : 'unknown'
 
-@Route('api/health')
-@Tags('health')
+type Health = {
+  version: string
+  status: 'ok'
+}
+
+@Route('health')
 @injectable()
 export class HealthController extends Controller {
-  private agent: Agent
-
-  public constructor(agent: Agent) {
+  public constructor() {
     super()
-    this.agent = agent
   }
 
   /**
    * @summary Check health of API and its dependencies
    */
   @SuccessResponse(200)
+  @Hidden()
   @Get('/')
-  public async get() {
+  public async get(): Promise<Health> {
     return {
       status: 'ok',
       version: packageVersion,
