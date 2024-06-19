9c059760dea9d23d0ceb075d5e1460691169512e
Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>
2024-06-19 08:54:56 +0200

    updated

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,7 @@
-import { runRestAgent, type InboundTransport, type Transports, type AriesRestConfig } from './cliAgent.js'
+import { runRestAgent, type Transports, type AriesRestConfig } from './cliAgent.js'
 import type { AskarWalletPostgresStorageConfig } from '@credo-ts/askar'
 import { Env } from './env.js'
 import { container } from 'tsyringe'
-import * as envalid from 'envalid'
 const env = container.resolve(Env)
 // const splitFlat = (i: string[]) => i.map((i) => i.split(' ')).flat()
 
@@ -179,68 +178,41 @@ const env = container.resolve(Env)
 //   .config()
 //   .env('AFJ_REST')
 //   .parseSync()
-const stringToArray = (input:string,allowedVals:string[]|''='')=> {
-  console.log(input)
-  console.log(allowedVals)
-  const arr = input
-    .split(',')
-    .map((s) => s.trim())
-    .filter((s) => !!s)
-  console.log('arr')
-  console.log(arr)
-
-    if (allowedVals===''){
-    const isValid = arr.every(item => allowedVals.includes(item));
-  
-    if (!isValid) {
-      throw new Error(`Array can only contain ${allowedVals.toString()}`);
-    }
-  }
-    if (arr.length === 0 || arr.length > 2) {
-      console.log('console logging')
-      console.log(input)
-      console.log(allowedVals)
-      throw new Error(`Array must contain at least one and at most two elements, you provided: ${input}`);
-    }
-  
-
-return arr as Transports[]
-}
 
 export async function runCliServer() {
   await runRestAgent({
     label: env.get('LABEL'),
     walletConfig: {
-      id:  env.get('WALLET_ID'),
-      key:  env.get('WALLET_KEY'),
+      id: env.get('WALLET_ID'),
+      key: env.get('WALLET_KEY'),
       storage:
-      env.get('STORAGE_TYPE') === 'sqlite'
+        env.get('STORAGE_TYPE') === 'sqlite'
           ? {
               type: 'sqlite',
             }
           : ({
               type: 'postgres',
               config: {
-                host: `${ env.get('POSTGRES_HOST') as string}:${ env.get('POSTGRES_PORT') as string}`,
+                host: `${env.get('POSTGRES_HOST') as string}:${env.get('POSTGRES_PORT') as string}`,
               },
               credentials: {
-                account:  env.get('POSTGRES_USERNAME') as string,
-                password:  env.get('POSTGRES_PASSWORD') as string,
+                account: env.get('POSTGRES_USERNAME') as string,
+                password: env.get('POSTGRES_PASSWORD') as string,
               },
             } satisfies AskarWalletPostgresStorageConfig),
     },
-    endpoints:  env.get('ENDPOINT'),
-    autoAcceptConnections:  env.get('AUTO_ACCEPT_CONNECTIONS'),
+    endpoints: env.get('ENDPOINT'),
+    autoAcceptConnections: env.get('AUTO_ACCEPT_CONNECTIONS'),
     autoAcceptCredentials: env.get('AUTO_ACCEPT_CREDENTIALS'),
     autoAcceptProofs: env.get('AUTO_ACCEPT_PROOFS'),
     autoUpdateStorageOnStartup: env.get('AUTO_UPDATE_STORAGE_ON_STARTUP'),
-    backupBeforeStorageUpdate: env.get('BACKUP_BEFORE_STORAGE_UPDATE') ,
+    backupBeforeStorageUpdate: env.get('BACKUP_BEFORE_STORAGE_UPDATE'),
     autoAcceptMediationRequests: env.get('AUTO_ACCEPT_MEDIATION_REQUESTS'),
     useDidKeyInProtocols: env.get('USE_DID_KEY_IN_PROTOCOLS'),
     useDidSovPrefixWhereAllowed: env.get('USE_DID_SOV_PREFIX_WHERE_ALLOWED'),
     logLevel: env.get('LOG_LEVEL'),
     inboundTransports: env.get('INBOUND_TRANSPORT'),
-    outboundTransports: env.get('OUTBOUND_TRANSPORT'), 
+    outboundTransports: env.get('OUTBOUND_TRANSPORT'),
     connectionImageUrl: env.get('CONNECTION_IMAGE_URL'),
     webhookUrl: env.get('WEBHOOK_URL'),
     adminPort: env.get('ADMIN_PORT'),
@@ -249,10 +221,10 @@ export async function runCliServer() {
     personaTitle: env.get('PERSONA_TITLE'),
     personaColor: env.get('PERONA_COLOR'),
     verifiedDrpcOptions: {
-      "proofTimeoutMs": env.get('VERIFIED_DRPC_OPTOPNS_PROOF_TIMEOUT_MS'),
-      "requestTimeoutMs": env.get('VERIFIED_DRPC_OPTIONS_REQUEST_TIMEOUT_MS'),
-      "credDefId": env.get('VERIFIED_DRPC_OPTIONS_CRED_DEF_ID'),
-      "proofRequestOptions": env.get('VERIFIED_DRPC_OPTIONS_PROOF_REQUEST_OPTIONS')
+      proofTimeoutMs: env.get('VERIFIED_DRPC_OPTOPNS_PROOF_TIMEOUT_MS'),
+      requestTimeoutMs: env.get('VERIFIED_DRPC_OPTIONS_REQUEST_TIMEOUT_MS'),
+      credDefId: env.get('VERIFIED_DRPC_OPTIONS_CRED_DEF_ID'),
+      proofRequestOptions: env.get('VERIFIED_DRPC_OPTIONS_PROOF_REQUEST_OPTIONS'),
     },
   } as AriesRestConfig)
 }
