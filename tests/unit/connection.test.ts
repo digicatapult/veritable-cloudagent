39a9a71a9727512493316bff2dfce2aaae3e7df6
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-08-05 15:20:03 +0100

    test for 400

--- tests/unit/connection.test.ts
+++ tests/unit/connection.test.ts
@@ -307,8 +307,8 @@ describe('ConnectionController', () => {
       await request(app).post('/v1/connections').send({ did: 'bla' }).expect(422)
     })
 
-    it('500s if DID is invalid', async function () {
-      await request(app).post('/v1/connections').send({ did: 'did:bla' }).expect(500)
+    it.only('400s if DID is invalid', async function () {
+      await request(app).post('/v1/connections').send({ did: 'did:bla' }).expect(400)
     })
   })
 
