fb4c0f2019c31aca0cc8037bc0a05c6223645334
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-08-06 13:36:55 +0100

    add handling for connectiondidrotated

--- src/events/ConnectionEvents.ts
+++ src/events/ConnectionEvents.ts
@@ -1,33 +1,58 @@
-import { type Agent, type ConnectionStateChangedEvent, ConnectionEventTypes } from '@credo-ts/core'
+import {
+  type Agent,
+  type ConnectionStateChangedEvent,
+  ConnectionDidRotatedEvent,
+  ConnectionEventTypes,
+} from '@credo-ts/core'
 
 import type { ServerConfig } from '../utils/ServerConfig.js'
 import { sendWebSocketEvent } from './WebSocketEvents.js'
 import { sendWebhookEvent } from './WebhookEvent.js'
 
 export const connectionEvents = async (agent: Agent, config: ServerConfig) => {
-  agent.events.on(
-    ConnectionEventTypes.ConnectionStateChanged || ConnectionEventTypes.ConnectionDidRotated,
-    async (event: ConnectionStateChangedEvent) => {
-      const record = event.payload.connectionRecord
-      const body = record.toJSON()
+  agent.events.on(ConnectionEventTypes.ConnectionStateChanged, async (event: ConnectionStateChangedEvent) => {
+    const record = event.payload.connectionRecord
+    const body = record.toJSON()
 
-      // Only send webhook if webhook url is configured
-      if (config.webhookUrl) {
-        for (const webhookUrl of config.webhookUrl) {
-          sendWebhookEvent(webhookUrl + '/connections', body, agent.config.logger)
-        }
-      }
-
-      if (config.socketServer) {
-        // Always emit websocket event to clients (could be 0)
-        sendWebSocketEvent(config.socketServer, {
-          ...event,
-          payload: {
-            ...event.payload,
-            connectionRecord: body,
-          },
-        })
+    // Only send webhook if webhook url is configured
+    if (config.webhookUrl) {
+      for (const webhookUrl of config.webhookUrl) {
+        sendWebhookEvent(webhookUrl + '/connections', body, agent.config.logger)
       }
     }
-  )
+
+    if (config.socketServer) {
+      // Always emit websocket event to clients (could be 0)
+      sendWebSocketEvent(config.socketServer, {
+        ...event,
+        payload: {
+          ...event.payload,
+          connectionRecord: body,
+        },
+      })
+    }
+  })
+
+  agent.events.on(ConnectionEventTypes.ConnectionDidRotated, async (event: ConnectionDidRotatedEvent) => {
+    const record = event.payload.connectionRecord
+    const body = record.toJSON()
+
+    // Only send webhook if webhook url is configured
+    if (config.webhookUrl) {
+      for (const webhookUrl of config.webhookUrl) {
+        sendWebhookEvent(webhookUrl + '/connections', body, agent.config.logger)
+      }
+    }
+
+    if (config.socketServer) {
+      // Always emit websocket event to clients (could be 0)
+      sendWebSocketEvent(config.socketServer, {
+        ...event,
+        payload: {
+          ...event.payload,
+          connectionRecord: body,
+        },
+      })
+    }
+  })
 }
