c14c051000bbed1eebf0403d6ef17d43f02ea3a4
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-08-06 21:25:40 +0100

    better return handling for both event types

--- src/events/ConnectionEvents.ts
+++ src/events/ConnectionEvents.ts
@@ -1,7 +1,7 @@
 import {
   type Agent,
+  type ConnectionDidRotatedEvent,
   type ConnectionStateChangedEvent,
-  ConnectionDidRotatedEvent,
   ConnectionEventTypes,
 } from '@credo-ts/core'
 
@@ -10,9 +10,15 @@ import { sendWebSocketEvent } from './WebSocketEvents.js'
 import { sendWebhookEvent } from './WebhookEvent.js'
 
 export const connectionEvents = async (agent: Agent, config: ServerConfig) => {
-  agent.events.on(ConnectionEventTypes.ConnectionStateChanged, async (event: ConnectionStateChangedEvent) => {
-    const record = event.payload.connectionRecord
-    const body = record.toJSON()
+  const eventHandler = async (event: ConnectionStateChangedEvent | ConnectionDidRotatedEvent) => {
+    const body = event.payload
+
+    // The UI only needs to know if the DID has been rotated to 0 i.e. disconnected
+    if (event.type === ConnectionEventTypes.ConnectionDidRotated) {
+      if (!event.payload.theirDid?.to) {
+        agent.config.logger.info(`DID rotated to falsy type`)
+      }
+    }
 
     // Only send webhook if webhook url is configured
     if (config.webhookUrl) {
@@ -26,33 +32,11 @@ export const connectionEvents = async (agent: Agent, config: ServerConfig) => {
       sendWebSocketEvent(config.socketServer, {
         ...event,
         payload: {
-          ...event.payload,
-          connectionRecord: body,
+          ...body,
         },
       })
     }
-  })
-
-  agent.events.on(ConnectionEventTypes.ConnectionDidRotated, async (event: ConnectionDidRotatedEvent) => {
-    const record = event.payload.connectionRecord
-    const body = record.toJSON()
-
-    // Only send webhook if webhook url is configured
-    if (config.webhookUrl) {
-      for (const webhookUrl of config.webhookUrl) {
-        sendWebhookEvent(webhookUrl + '/connections', body, agent.config.logger)
-      }
-    }
-
-    if (config.socketServer) {
-      // Always emit websocket event to clients (could be 0)
-      sendWebSocketEvent(config.socketServer, {
-        ...event,
-        payload: {
-          ...event.payload,
-          connectionRecord: body,
-        },
-      })
-    }
-  })
+  }
+  agent.events.on(ConnectionEventTypes.ConnectionStateChanged, eventHandler)
+  agent.events.on(ConnectionEventTypes.ConnectionDidRotated, eventHandler)
 }
