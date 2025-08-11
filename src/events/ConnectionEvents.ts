e4f4de6b5c9d41f378d0f24cd652fe6afcb878f7
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-08-11 13:20:39 +0100

    template literals for webhookurl

--- src/events/ConnectionEvents.ts
+++ src/events/ConnectionEvents.ts
@@ -24,7 +24,7 @@ export const connectionEvents = async (agent: Agent, config: ServerConfig) => {
     // Only send webhook if webhook url is configured
     if (config.webhookUrl) {
       for (const webhookUrl of config.webhookUrl) {
-        sendWebhookEvent(webhookUrl + '/connections', body, agent.config.logger)
+        sendWebhookEvent(`${webhookUrl}/connections`, body, agent.config.logger)
       }
     }
 
