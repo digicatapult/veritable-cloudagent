04192a8a60e4f671419624810d18142d2643a858
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-08-13 09:39:38 +0100

    improved hangup/deletion logic

--- src/controllers/v1/connections/ConnectionController.ts
+++ src/controllers/v1/connections/ConnectionController.ts
@@ -126,11 +126,21 @@ export class ConnectionController extends Controller {
   public async closeConnection(@Request() req: express.Request, @Path('connectionId') connectionId: string) {
     const deleteConnectionRecord: boolean = req.query.delete === 'true'
     try {
-      this.setStatus(204)
-      await this.agent.connections.hangup({ connectionId: connectionId, deleteAfterHangup: deleteConnectionRecord })
-      req.log.info('%s disconnected', connectionId)
-      if (deleteConnectionRecord === true) {
-        req.log.info('%s record deleted', connectionId)
+      const connectionRecord = await this.agent.connections.getById(connectionId)
+      if (!connectionRecord.theirDid) {
+        // If the connectionRecord doesn't have a theirDid, they've previously
+        // disconnected from us, or the connection is incomplete
+        if (deleteConnectionRecord) {
+          this.setStatus(204)
+          await this.agent.connections.deleteById(connectionId)
+          req.log.info('%s record deleted', connectionId)
+        } else {
+          throw new BadRequest('cannot hangup on connection without a theirDid')
+        }
+      } else {
+        this.setStatus(204)
+        await this.agent.connections.hangup({ connectionId: connectionId, deleteAfterHangup: deleteConnectionRecord })
+        req.log.info(`${connectionId}${deleteConnectionRecord ? ' disconnected' : ' disconnected and deleted'}`)
       }
     } catch (error) {
       if (error instanceof RecordNotFoundError) {
