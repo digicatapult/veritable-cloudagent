f5d8ef2a3fe1e026d7d3d0187a31ffd9524e9dcb
Jonathan Gray <jonmattgray@gmail.com>
2025-09-17 12:21:16 +0100

    better server, graceful exit and logging

--- src/didweb/server.ts
+++ src/didweb/server.ts
@@ -1,10 +1,10 @@
 import cors from 'cors'
 import express from 'express'
 import fs from 'fs'
+import type { Server } from 'http'
 import https from 'https'
-import { createRequestLogger } from '../utils/logger.js'
-
 import { Logger } from 'pino'
+import { createRequestLogger } from '../utils/logger.js'
 import { errorHandler } from './error.js'
 
 export interface DidWebServerConfig {
@@ -17,14 +17,13 @@ export interface DidWebServerConfig {
 
 export class DidWebServer {
   private app: express.Application
-  private server?: https.Server
+  private server?: Server
   private logger: Logger
   private config: DidWebServerConfig
 
   constructor(logger: Logger, config: DidWebServerConfig) {
     this.logger = logger
     this.config = config
-
     this.app = express()
     this.setupRoutes()
   }
@@ -32,9 +31,7 @@ export class DidWebServer {
   private setupRoutes(): void {
     this.app.use(createRequestLogger(this.logger))
     this.app.use(cors())
-
     this.app.get('/health', this.healthCheck)
-
     this.app.use(errorHandler)
   }
 
@@ -45,27 +42,39 @@ export class DidWebServer {
   async start(): Promise<void> {
     if (!this.config.enabled) {
       this.logger.info('DID:web server disabled')
-      return
-    }
-    if (!this.config.useDevCert) {
-      this.app.listen(this.config.port, () => {
-        this.logger.info(`DID:web server started on http port ${this.config.port}`)
-      })
-      return
-    }
-    let httpsCredentials
-    try {
-      httpsCredentials = {
-        cert: fs.readFileSync(this.config.certPath),
-        key: fs.readFileSync(this.config.keyPath),
-      }
-    } catch {
-      throw new Error('Failed to read certificate or key file. Have you generated them?')
     }
 
-    this.server = https.createServer(httpsCredentials, this.app)
-    this.server.listen(this.config.port, () => {
-      this.logger.info(`DID:web server started on https port ${this.config.port}`)
-    })
+    const setupGracefulExit = (sigName: NodeJS.Signals, server: Server, exitCode: number) => {
+      process.on(sigName, async () => {
+        server.close(() => {
+          process.exit(exitCode)
+        })
+      })
+    }
+
+    if (this.config.useDevCert) {
+      let httpsCredentials
+      try {
+        httpsCredentials = {
+          cert: fs.readFileSync(this.config.certPath),
+          key: fs.readFileSync(this.config.keyPath),
+        }
+      } catch {
+        throw new Error(
+          `Failed to read certificate or key file. certPath: ${this.config.certPath}, keyPath: ${this.config.keyPath}`
+        )
+      }
+      this.server = https.createServer(httpsCredentials, this.app)
+      this.server.listen(this.config.port, () => {
+        this.logger.info(`DID:web server started on https port ${this.config.port}`)
+      })
+    } else {
+      this.server = this.app.listen(this.config.port, () => {
+        this.logger.info(`DID:web server started on http port ${this.config.port}`)
+      })
+    }
+
+    setupGracefulExit('SIGINT', this.server, 0)
+    setupGracefulExit('SIGTERM', this.server, 143)
   }
 }
