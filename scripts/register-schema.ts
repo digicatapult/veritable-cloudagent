c7f4863e8f1d2501e65c371ac75c68f305024357
Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>
2025-10-14 15:14:13 +0100

    add line break

--- scripts/register-schema.ts
+++ scripts/register-schema.ts
@@ -151,8 +151,8 @@ async function main() {
   }
 
   // Output (stdout) schemaId first line, credential definition (if any) second line.
-  process.stdout.write(` Schema id: ${schemaId}\n`)
-  if (credDefId) process.stdout.write(`Credential definition: ${credDefId}\n`)
+  process.stdout.write(`Schema id: \n${schemaId}\n`)
+  if (credDefId) process.stdout.write(`Credential definition id: \n${credDefId}\n`)
 }
 
 main().catch((e) => process.stderr.write((e as Error).stack + '\n') && process.exit(1))
