578dd74ed2544cf3fcb7ab3874bd407de3b113d7
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2026-01-10 19:21:59 +0000

    remove comment

--- scripts/register-schema.ts
+++ scripts/register-schema.ts
@@ -83,7 +83,6 @@ function parseArgs(argv: string[]): ParsedArgs {
 
 async function main() {
   const { schemaFileName, issuerId, baseUrl } = parseArgs(process.argv)
-  // if (!schemaFileName) throw new Error('Schema filename argument required, e.g. `makeAuthorisation.json`')
 
   const schemaPath = path.join(__dirname, 'schemas', `${schemaFileName}`)
   if (!fs.existsSync(schemaPath)) throw new Error(`Schema definition not found: ${schemaPath}`)
