b7388c1bccd0ecfb39efcf7e0297f7599ebac9c1
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2026-01-16 20:27:42 +0000

    reorg

--- src/controllers/types/connection.ts
+++ src/controllers/types/connection.ts
@@ -4,18 +4,6 @@
 import type { HandshakeProtocol, OutOfBandDidCommService, ReceiveOutOfBandInvitationConfig } from '@credo-ts/core'
 import type { DID, UUID } from './common.js'
 
-export interface OutOfBandInvitationSchema {
-  '@id'?: UUID
-  '@type': string
-  label: string
-  goalCode?: string
-  goal?: string
-  accept?: string[]
-  handshake_protocols?: HandshakeProtocol[]
-  services: Array<OutOfBandDidCommService | string>
-  imageUrl?: string
-}
-
 type ReceiveOutOfBandInvitationProps = Omit<ReceiveOutOfBandInvitationConfig, 'routing'>
 
 export interface ReceiveInvitationProps extends ReceiveOutOfBandInvitationProps {
@@ -33,6 +21,18 @@ export interface AcceptInvitationConfig {
   alias?: string
   imageUrl?: string
   mediatorId?: string
+}
+
+export interface OutOfBandInvitationSchema {
+  '@id'?: UUID
+  '@type': string
+  label: string
+  goalCode?: string
+  goal?: string
+  accept?: string[]
+  handshake_protocols?: HandshakeProtocol[]
+  services: Array<OutOfBandDidCommService | string>
+  imageUrl?: string
 }
 
 export interface ConnectionInvitationSchema {
