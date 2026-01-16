2a9758eb629a80fabe7165c2b2b0e3848e87cc4a
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2026-01-16 10:55:15 +0000

    simplify didwebServer test suite

--- tests/integration/e2e.didwebServer.test.ts
+++ tests/integration/e2e.didwebServer.test.ts
@@ -1,37 +1,26 @@
 import { expect } from 'chai'
-import { readFileSync } from 'fs'
-import { Agent, fetch } from 'undici'
-
-const caPath = process.env.NODE_EXTRA_CA_CERTS || '/rootCA.pem'
-const ca = readFileSync(caPath)
+import { fetch } from 'undici'
 
 const agents = ['https://alice:8443', 'https://bob:8443', 'https://charlie:8443']
 const dids = ['did:web:alice%3A8443', 'did:web:bob%3A8443', 'did:web:charlie%3A8443']
-const httpsAgent = new Agent({
-  connect: {
-    ca: ca,
-  },
-})
 
 describe('DID:web server', function () {
-  agents.forEach((baseUrl) => {
+  agents.forEach((baseUrl, index) => {
     it(`should return ok for ${baseUrl} DID:web health endpoint`, async function () {
-      const res = await fetch(`${baseUrl}/health`, { dispatcher: httpsAgent })
+      const res = await fetch(`${baseUrl}/health`)
       const body = await res.json()
       expect(body).to.have.property('status', 'ok')
     })
-  })
 
-  agents.forEach((baseUrl, index) => {
     it(`should return test DID from ${baseUrl}`, async function () {
-      const res = await fetch(`${baseUrl}/did.json`, { dispatcher: httpsAgent })
+      const res = await fetch(`${baseUrl}/did.json`)
       const body = await res.json()
       expect(body).to.deep.include({ id: dids[index] })
     })
-  })
 
-  it(`should return 404 for non-existent DID`, async function () {
-    const res = await fetch(`${agents[0]}/non-existent/did.json`, { dispatcher: httpsAgent })
-    expect(res.status).to.equal(404)
+    it(`should return 404 for non-existent DID on ${baseUrl}`, async function () {
+      const res = await fetch(`${baseUrl}/non-existent/did.json`)
+      expect(res.status).to.equal(404)
+    })
   })
 })
